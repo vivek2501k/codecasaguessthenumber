6280c5868c31ae65b24450d9ca152d712ccaf266
Vivek Kumar <84407994+vivek2501k@users.noreply.github.com>
2023-07-17 19:43:42 +0530

    Add files via upload

---
+++ GuessTheNumber.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+        int guess = 0;
+
+        System.out.println("Welcome to Guess the Number!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+        while (guess != secretNumber) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts!");
+            }
+        }
+
+        scanner.close();
+    }
+}
